88440e81b4b2a1225547655815cbac9cd1d0057f
Aaron Gonzales <agonzales@cs.unm.edu>
2015-04-04 12:42:01 -0600

    put fft extraction in a file; need to do mcg

--- src/fft.py
+++ src/fft.py
@@ -1,6 +1,32 @@
+import glob2  # needed for recursive glob
+from scipy.io.wavfile import read
 import scipy as sp
+import numpy as np
 
-fn = 'filename'
 
-sample_rate X = sp.io.wavfile.read(fn)
-fft_features = abs(sp.fft(X)[:1000])
+def _extract_fft(filename):
+    """Extracts the ffyfeatures from a file and saves them
+        in numpy binary format"""
+    savename = filename[0: len(filename)-4] + '.fft'
+    samp_rate, X = read(filename)
+    a = sp.fft(X, n=1000)
+    np.save(savename, a)
+
+
+def _extract_mcc(filename):
+    """Extracts """
+    savename = filename[0: len(filename)-4] + '.mc'
+    samp_rate, X = read(filename)
+    a = sp.fft(X, n=1000)
+    np.save(savename, a)
+
+
+def extract_feats(path='/cs/genres/*/*.wav', feature='fft'):
+    """Extracts features from a  """
+    classes = [line for line in glob2.glob(path)]
+    for line in classes:
+        print('extracting fft from %s' % line)
+        if feature == 'fft':
+            _extract_fft(line)
+        if feature == 'mcc':
+            _extract_mcc(line)
